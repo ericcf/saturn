d296cf490046234030c8ed0ec46f31eba12a21ce
Eric Carty-Fickes <ericcf@northwestern.edu>
2010-11-23 16:34:30 -0600

    improved nav tab highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
 
   def nav_item(title, path, html_options={}, action=nil, klass=nil)
     return if action && klass && cannot?(action, klass)
-    li_class = [(current_page?(path) ? "active" : ""), html_options[:class]]
+    li_class = [(path_match?(path) ? "active" : ""), html_options[:class]]
     "<li class=\"#{li_class.compact.join(" ")}\">" +
-    link_to(title, path, :class => current_page?(path) ? "current" : "") +
+    link_to(title, path, :class => path_match?(path) ? "current" : "") +
     "</li>"
   end
 
@@ -15,5 +15,14 @@ module ApplicationHelper
        options.include?(:without_day) ? nil : date.strftime("%a"),
       "#{date.month}/#{date.day}"
     ].compact.join(" ")
+  end
+
+  private
+
+  def path_match?(path)
+    if path == "/"
+      return request.path == "/"
+    end
+    !(request.path =~ /^#{path}/).nil?
   end
 end
